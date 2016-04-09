84c00141ddfed0418c21b732b2d45b8431c55fef
corinne krych <corinnekrych@gmail.com>
2016-04-09 16:58:58 +0200

    Layout

--- DoItCoach WatchKit App Extension/InterfaceController.swift
+++ DoItCoach WatchKit App Extension/InterfaceController.swift
@@ -11,11 +11,28 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
+    @IBOutlet var taskNameLabel: WKInterfaceLabel!
+    @IBOutlet var group: WKInterfaceGroup!
+    @IBOutlet var startButton: WKInterfaceButton!
+    @IBOutlet var startButtonImage: WKInterfaceImage!    
+    @IBOutlet var timer: WKInterfaceTimer!
+    
+    @IBAction func onStartButton() {
+        guard let currentTask = TasksManager.instance.currentTask else {return} // [1]
+        if !currentTask.isStarted() { // [2]
+            let duration = NSDate(timeIntervalSinceNow: currentTask.duration)
+            timer.setDate(duration)
+            timer.start() // [3]
+            currentTask.start()
+            startButtonImage.setHidden(true) // [4]
+            timer.setHidden(false) // [5]
+            taskNameLabel.setText(currentTask.name)
+        }
+    }
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
-        // Configure interface objects here.
+        display(TasksManager.instance.currentTask)
     }
 
     override func willActivate() {
@@ -26,6 +43,16 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    func display(task: Task?) {
+        guard let task = task else { // [1]
+            taskNameLabel.setText("NOTHING TO DO :)")
+            timer.setHidden(true)
+            startButtonImage.setHidden(true)
+            return
+        }
+        taskNameLabel.setText(task.name) // [2]
     }
 
 }
